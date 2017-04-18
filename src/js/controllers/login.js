4e33828030d33b816bc6277270d5679e5dc4bd56
Ben Currie <benjamin.w.currie@gmail.com>
2017-04-18 17:25:34 +0100

    created logo and made a few stylistic changes

--- src/js/controllers/login.js
+++ src/js/controllers/login.js
@@ -2,27 +2,27 @@ angular
   .module('oddJob')
   .controller('LoginCtrl', LoginCtrl);
 
-LoginCtrl.$inject = ['$auth'];
-function LoginCtrl($auth) {
+LoginCtrl.$inject = ['$auth', '$state'];
+function LoginCtrl($auth, $state) {
   const vm = this;
 
   function register() {
     $auth.signup(vm.user)
-    .then(user => console.log(user));
+    .then(() => $state.go ('login'));
   }
 
   vm.register = register;
 
   function login() {
     $auth.login(vm.credentials)
-    .then(user => console.log(user));
+    .then(() => $state.go ('jobsIndex'));
   }
 
   vm.login = login;
 
   function authenticate(provider) {
     $auth.authenticate(provider)
-    .then(user => console.log(user));
+    .then(() => $state.go ('jobsIndex'));
   }
 
   vm.authenticate = authenticate;
